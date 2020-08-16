7f2b6b2defbb5e395241b56d135ba42048beb174
mkusaka <hinoshita1992@gmail.com>
2020-08-16 22:27:29 +0900

    feat: use typescript

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { Button } from './Button';
 import './header.css';
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
+type Props = {
+  user?: Record<string, unknown>;
+  onLogin: (...args: unknown[]) => void;
+  onLogout: (...args: unknown[]) => void;
+  onCreateAccount: (...args: unknown[]) => void;
+}
+
+export const Header: React.FC<Props> = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="wrapper">
       <div>
@@ -30,23 +36,13 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         {user ? (
           <Button size="small" onClick={onLogout} label="Log out" />
         ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+            <>
+              <Button size="small" onClick={onLogin} label="Log in" />
+              <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            </>
+          )}
       </div>
     </div>
   </header>
 );
 
-Header.propTypes = {
-  user: PropTypes.shape({}),
-  onLogin: PropTypes.func.isRequired,
-  onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
-
-Header.defaultProps = {
-  user: null,
-};
--- stories/Page.tsx
+++ stories/Page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { Header } from './Header';
 import './page.css';
 
-export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
+type Props = {
+  user?: Record<string, unknown>;
+  onLogin: (...args: unknown[]) => void;
+  onLogout: (...args: unknown[]) => void;
+  onCreateAccount: (...args: unknown[]) => void;
+}
+
+export const Page: React.FC<Props> = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <article>
     <Header user={user} onLogin={onLogin} onLogout={onLogout} onCreateAccount={onCreateAccount} />
 
@@ -59,13 +65,3 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
     </section>
   </article>
 );
-Page.propTypes = {
-  user: PropTypes.shape({}),
-  onLogin: PropTypes.func.isRequired,
-  onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
-
-Page.defaultProps = {
-  user: null,
-};
